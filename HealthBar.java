0572273b1e593634e8bfe49c01842f182fe61b19
Diegocoghlan <60474403+Diegocoghlan@users.noreply.github.com>
2020-05-27 05:03:30 -0500

    Add files via upload
    Barra de salud

---
+++ HealthBar.java
@@ -0,0 +1,34 @@
+import greenfoot.*;
+
+public class HealthBar extends Actor
+{
+    int health = 4;
+    int healthBarWidth = 80;
+    int healthBarHeight = 15;
+    int pixelsPerHealthPoint = (int)healthBarWidth/health;
+
+    public HealthBar()
+    {
+        update();
+    }
+
+    public void act() 
+    {
+        update();
+    }    
+
+    public void update()
+    {
+        setImage(new GreenfootImage(healthBarWidth + 2, healthBarHeight + 2));
+        GreenfootImage myImage = getImage();
+        myImage.setColor(Color.WHITE);
+        myImage.drawRect(0, 0, healthBarWidth + 1, healthBarHeight + 1);
+        myImage.setColor(Color.RED);
+        myImage.fillRect(1, 1, health*pixelsPerHealthPoint, healthBarHeight);
+    }
+
+    public void loseHealth()
+    {
+        health--;
+    }
+}
