2414207512fa445ca72d66cab071fc47829d2841
Diegocoghlan <60474403+Diegocoghlan@users.noreply.github.com>
2020-05-27 06:08:59 -0500

    Barra de salud a todas las misiones
    Se implementó la barra de salud a la clase padre de las misiones

--- GameWorld.java
+++ GameWorld.java
@@ -2,9 +2,22 @@ import greenfoot.*;
 
 public class GameWorld extends World
 {
+    HealthBar healthbar = new HealthBar();
+
     public GameWorld()
     {    
         super(1100, 500, 1, false);
+
+        prepare();
     }
-        
+    
+    private void prepare()
+    {
+        addObject(healthbar, 200, 40);        
+    }
+
+    public HealthBar getHealthBar()
+    {
+        return healthbar;
+    }        
 }
