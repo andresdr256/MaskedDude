ee742ac4daef800075c6f528438c934b2b5a6168
Diegocoghlan <60474403+Diegocoghlan@users.noreply.github.com>
2020-05-27 06:16:53 -0500

    Mostrar barra de salud encima del fondo
    Agregar la barra de salud después del fondo para que siempre se vea

--- GameWorld.java
+++ GameWorld.java
@@ -7,15 +7,8 @@ public class GameWorld extends World
     public GameWorld()
     {    
         super(1100, 500, 1, false);
-
-        prepare();
     }
     
-    private void prepare()
-    {
-        addObject(healthbar, 200, 40);        
-    }
-
     public HealthBar getHealthBar()
     {
         return healthbar;
